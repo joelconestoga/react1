89a6054b46858e1253024c94e696f5b75ef062fa
Joel Matsu <joel.matsu@sera4.com>
2020-03-29 17:58:02 -0400

    LIFT THE STATE UP from Counters to Main so NavBar can also have access to the state

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,43 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 33 },
-      { id: 4, value: 0 }
-    ]
-  };
-  handleIncrement = counterToIncrement => {
-    const clonedCounters = [...this.state.counters];
-    const index = clonedCounters.indexOf(counterToIncrement);
-    clonedCounters[index] = { ...counterToIncrement };
-    clonedCounters[index].value++;
-    this.setState({ counters: clonedCounters });
-  };
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
   render() {
     return (
       <div>
-        <button onClick={this.handleReset} className="btn btn-primary btn-sm">
+        <button onClick={this.props.onReset} className="btn btn-primary btn-sm">
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
