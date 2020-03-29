9a168e8098cbfd68b826474fe46bc465d8eb867d
Joel Matsu <joel.matsu@sera4.com>
2020-03-29 14:34:16 -0400

    Created Reset Button, removed state from children so they can have a Single Source of Truth

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -10,17 +10,35 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+  handleIncrement = counterToIncrement => {
+    const clonedCounters = [...this.state.counters];
+    const index = clonedCounters.indexOf(counterToIncrement);
+    clonedCounters[index] = { ...counterToIncrement };
+    clonedCounters[index].value++;
+    this.setState({ counters: clonedCounters });
+  };
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters: counters });
   };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm">
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
