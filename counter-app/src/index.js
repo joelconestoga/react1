6c39617a18706084f19fce6d0f3220878d1985b8
Joel Matsu <joel.matsu@sera4.com>
2020-05-17 16:29:04 -0400

    Adding REDUX - testing SUBSCRIBE/DISPATCH and the actions implemented - redux is working, but not connected to react yet

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,6 +4,40 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import store from "./redux/store"
+
+store.subscribe(() => {
+  console.log("Store changed:", store.getState());
+});
+store.dispatch({
+  type: "addCounter",
+  payload: {id: 1, value: 4}
+});
+store.dispatch({
+  type: "addCounter",
+  payload: {id: 2, value: 0}
+});
+store.dispatch({
+  type: "addCounter",
+  payload: {id: 3, value: 33}
+});
+store.dispatch({
+  type: "addCounter",
+  payload: {id: 4, value: 0}
+});
+store.dispatch({
+  type: "handleIncrement",
+  payload: {id: 1}
+});
+store.dispatch({
+  type: "handleReset",
+  payload: {id: 3}
+});
+store.dispatch({
+  type: "handleDelete",
+  payload: {id: 2}
+});
+console.log("state ==> ", store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
