a4e18b624e0ed95bc689bf131aaa627068a60ce9
Joel Matsu <joel.matsu@sera4.com>
2020-05-18 12:30:16 -0400

    Adding IMMER - to keep object immutability and avoid 'spread' operator as it can get messy in the future, when many/nested updates

--- counter-app/src/redux/reducer.js
+++ counter-app/src/redux/reducer.js
@@ -1,13 +1,14 @@
 import * as actions from './actionTypes';
+import { produce } from 'immer';
 
 export default function reducer(state = [], action) {
   switch(action.type) {
     case actions.ADD_COUNTER:
       return [...state, {id: action.payload.id, value: action.payload.value}];
     case actions.HANDLE_INCREMENT:
-      return state.map(counter => counter.id !== action.payload.id ? counter : {...counter, value: counter.value + 1});
+      return state.map(counter => counter.id === action.payload.id ? produce(counter, draftCounter => { draftCounter.value += 1 }) : counter);
     case actions.HANDLE_RESET:
-      return state.map(counter => ({...counter, value: 0}));
+      return state.map(counter => produce(counter, draftCounter => { draftCounter.value = 0 }));
     case actions.HANDLE_DELETE:
       return state.filter(counter => counter.id !== action.payload.id);
     default:
